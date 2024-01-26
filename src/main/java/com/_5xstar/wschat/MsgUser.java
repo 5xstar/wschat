d05a146f723c3a7ba3126b55a8703c4a50fefa03
5xstar <543296087@qq.com>
2024-01-26 08:19:28 +0800

    update 2024-1-26

--- src/main/java/com/_5xstar/wschat/MsgUser.java
+++ src/main/java/com/_5xstar/wschat/MsgUser.java
@@ -1,5 +1,8 @@
 package com._5xstar.wschat;
 
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.annotation.JSONField;
+
 /**
  * 消息用户
  * 庞海文 2024-1-23
@@ -8,18 +11,21 @@ public class MsgUser {
     /**
      * 服务器唯一标识
      */
+    @JSONField
     public String serverName;
     /**
      * 在serverName中的唯一标识
      */
+    @JSONField
     public String roomName;
     /**
      * 在roomName中的唯一标识
      */
+    @JSONField
     public String userName;
 
     @Override
     public String toString(){
-        return "serverName=" + serverName + " roomName="+roomName+" userName="+userName;
+        return JSON.toJSONString(this);
     }
 }
